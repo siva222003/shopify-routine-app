bbe32426ff1ff9dc7bf896460befad3765bae295
siva222003 <sivapidugu02@gmail.com>
2024-11-08 01:19:19 +0530

    fix: solve api export

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+export const api = axios.create({
+  baseURL: process.env.API_URL,
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Y2Q4MGQxMDNiZTYxNjRkMzgyOWMwYiIsImlhdCI6MTcyODU5MjA3NywiZXhwIjoxNzM2MzY4MDc3fQ.ioD-bcugzIAKGnsrFJz4mNPb2_3wEMfdHmrZ41kvmu4",
+  },
+});
+
 export const userApi = axios.create({
   baseURL: process.env.CUSTOMER_API_URL,
   headers: {
